e3b63e76d0116dde80cf3489bb53dfd2086efef2
Abdul Samad <ibrahimabduldamad911@gmail.com>
2023-10-24 11:12:50 +0100

    removed no longer needed features and made code cleaner

--- users/urls.py
+++ users/urls.py
@@ -3,9 +3,9 @@ from . import views
 
 urlpatterns = [
     path('login/', views.loginUser, name='login-user'),
-    path('<str:pk>/', views.about, name='about'),
-    path('user/setting/', views.setting, name='setting'),
-    path('user/logout/', views.logoutView, name='logout'),
+    path('user/<str:pk>/', views.about, name='about'),
+    path('setting/', views.setting, name='setting'),
+    path('logout/', views.logoutView, name='logout'),
     path('register/', views.register, name='register'),
     path('password-change/', views.changePassword, name='password-change'),
 
